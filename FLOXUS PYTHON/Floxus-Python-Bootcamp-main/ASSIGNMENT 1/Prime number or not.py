ce3f3bc7f4dce786cd7dfeacdaaaf8c4ccf2a77e
Abarna Pandian <abarnapandian2000@gmail.com>
2021-07-17 17:42:46 +0530

    Delete FLOXUS PYTHON/Floxus-Python-Bootcamp-main directory

--- FLOXUS PYTHON/Floxus-Python-Bootcamp-main/ASSIGNMENT 1/Prime number or not.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Enter a number : "))
-for i in range(2,num):
-    if(num % i == 0):
-        print("Its not a Prime Number")
-        break
-else:
-    print("Its a Prime Number")
--- FLOXUS PYTHON/Floxus-Python-Bootcamp-main/ASSIGNMENT 1/sum of n natural number using while loop.py
+++
@@ -1,7 +0,0 @@
-n = int (input("Enter a number : "))
-sum = 0
-i = 1
-while i<=n:
-    sum = sum + i
-    i = i + 1
-print("The sum of n natural number is : ",sum)
